d51ab74247e1f7d1fc5be5a5ee5be19b66b77c48
magicianjarden <116777564+magicianjarden@users.noreply.github.com>
2024-11-18 19:09:45 -0800

    Update route.ts

--- src/app/api/convert/document/route.ts
+++ src/app/api/convert/document/route.ts
@@ -17,7 +17,6 @@ import { nanoid } from 'nanoid';
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
 export const maxDuration = 300;
-export const bodyParser = false;
 
 export async function POST(req: NextRequest) {
   try {
