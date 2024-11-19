7f109c4f8f9434e4b49c15956e95088904f6d098
magicianjarden <116777564+magicianjarden@users.noreply.github.com>
2024-11-18 19:50:50 -0800

    ...

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    appDir: true,
-  },
-  swcMinify: false,
 }
 
 module.exports = nextConfig 
