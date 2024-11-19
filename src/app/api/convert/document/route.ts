530f387c9d4476654eb1a99d0dfc2bbfdd6fd4b0
magicianjarden <116777564+magicianjarden@users.noreply.github.com>
2024-11-18 21:06:46 -0800

    route timeout

--- src/app/api/convert/document/route.ts
+++ src/app/api/convert/document/route.ts
@@ -16,7 +16,7 @@ import { nanoid } from 'nanoid';
 
 export const runtime = 'nodejs';
 export const dynamic = 'force-dynamic';
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 export async function POST(req: NextRequest) {
   try {
