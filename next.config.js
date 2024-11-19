a6b3debe636b140b493523fb631e2fb9184eae3a
magicianjarden <116777564+magicianjarden@users.noreply.github.com>
2024-11-18 19:21:17 -0800

    small fixes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    serverActions: true
-  },
   distDir: '.next',
-  async redirects() {
-    return [];
-  }
 }
 
 module.exports = nextConfig 
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h2 className="text-2xl font-bold">404 - Page Not Found</h2>
-      <p className="mt-4">The page you're looking for doesn't exist.</p>
+      <p className="mt-4">The page you&apos;re looking for doesn&apos;t exist.</p>
       <Link 
         href="/"
         className="mt-4 text-blue-500 hover:text-blue-600"
