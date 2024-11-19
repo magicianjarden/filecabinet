275afa77b49265be25a2c9311da9d2de450f0630
magicianjarden <116777564+magicianjarden@users.noreply.github.com>
2024-11-18 20:12:36 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <TooltipProvider>
+          {children}
+        </TooltipProvider>
       </body>
     </html>
   )
