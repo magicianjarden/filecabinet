5cef50af9b405f3e55434dcbd15c944d0dd81dde
magicianjarden <116777564+magicianjarden@users.noreply.github.com>
2024-11-22 15:31:44 -0800

    direct issue fix

--- src/config/supported-formats.ts
+++ src/config/supported-formats.ts
@@ -1,9 +1,21 @@
-import { SupportedFormats } from '@/types/formats';
+import { SupportedFormats, FileCategory, FormatConfig } from './types/formats';
 
 export const supportedFormats: SupportedFormats = {
   archives: {
     input: ['zip'],
     output: ['zip']
+  },
+  presentations: {
+    input: ['pptx', 'ppt'],
+    output: ['pptx', 'pdf']
+  },
+  spreadsheets: {
+    input: ['xlsx', 'xls', 'csv'],
+    output: ['xlsx', 'csv']
+  },
+  ebooks: {
+    input: ['epub', 'mobi', 'pdf'],
+    output: ['epub', 'pdf']
   },
   code: {
     input: ['json', 'yaml', 'yml', 'xml', 'csv'],
